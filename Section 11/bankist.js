a78edcb42c9cc2ca42b875b96d539dae01f550af
sajanbhosale05 <83913731+sajanbhosale05@users.noreply.github.com>
2023-01-26 19:40:37 +0530

    Initial Commit1.1

--- Section 11/bankist.js
+++ Section 11/bankist.js
@@ -7,28 +7,28 @@
 // Data
 const account1 = {
   owner: "Sajan Bhosale",
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-  interestRate: 1.2, // %
+  movements: [20000, 4500, -400, 300000, -650, -130, 70, 130000],
+  interestRate: 2, // %
   pin: 1111,
 };
 
 const account2 = {
-  owner: "G N",
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  owner: "Gouri N",
+  movements: [50000, 34000, -150, -790, -3210, -1000, 85000, -30],
   interestRate: 1.5,
   pin: 2222,
 };
 
 const account3 = {
   owner: "Pavan Kshirsager",
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  movements: [20000, -200, 3400, -300, -20, 50, 4000, -460],
   interestRate: 0.7,
   pin: 3333,
 };
 
 const account4 = {
   owner: "Pratik Bahirwade",
-  movements: [430, 1000, 700, 50, 90],
+  movements: [4300, 10000, 7000, 50, 90],
   interestRate: 1,
   pin: 4444,
 };
@@ -76,8 +76,6 @@ const displayMovements = function (movements) {
   });
 };
 
-displayMovements(account1.movements);
-
 //Creating Usernames
 const createUser = function (account) {
   account.forEach((acc) => {
@@ -92,14 +90,25 @@ const createUser = function (account) {
 createUser(accounts);
 
 //display balance
-const calcBalanceDisplay = function (movements) {
-  const balance = movements.reduce(
+const calcBalanceDisplay = function (accounts) {
+  const balance = accounts.movements.reduce(
     (accumulator, movement) => accumulator + movement,
     0
   );
+  accounts.balance = balance;
   labelBalance.textContent = `₹${balance}`;
 };
 
+const updateUI = function (account) {
+  //DISPLAY MOVEMENTS OF CURRENT ACCOUNT
+  displayMovements(currentAccount.movements);
+  //DISPLAY BALANCE OF CURRENT ACCOUNT
+  calcBalanceDisplay(currentAccount);
+  //DISPLAY SUMMERY OF CURRENT ACCOUNT
+  calcDisplaySummery(currentAccount.movements);
+};
+
+// calcBalanceDisplay(account1.movements);
 const calcDisplaySummery = function (movement) {
   const incomes = movement
     .filter((movement) => movement > 0)
@@ -113,10 +122,79 @@ const calcDisplaySummery = function (movement) {
 
   const interest = movement
     .filter((movement) => movement > 0)
-    .map((deposits) => (deposits * 2) / 100)
+    .map((deposits) => (deposits * currentAccount.interestRate) / 100)
     .reduce((accumulator, interest) => accumulator + interest, 0);
 
   labelSumInterest.textContent = `₹${interest}`;
 };
 
-calcDisplaySummery(account1.movements);
+//Find Method:
+//find method work same as slice method but dont return array but a very first find(element satiesfying condition)
+// const fDeposit = account1.movements.find((movement) => movement > 0);
+// console.log(fDeposit);
+// console.log(account1.movements);
+
+// const account = accounts.find((acc) => acc.owner === "Sajan Bhosale");
+// console.log(account);
+
+//Implementing Authentication:
+let currentAccount;
+btnLogin.addEventListener("click", function (event) {
+  //This will prevent form from submitting
+  event.preventDefault();
+  currentAccount = accounts.find(
+    (account) => account.username === inputLoginUsername.value
+  );
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    //DISPLAY WELCOME MESSAGE
+    labelWelcome.textContent = `Welcome Back, ${
+      currentAccount.owner.split(" ")[0]
+    }`;
+    containerApp.style.opacity = 100;
+    //CLEAR INPUTS
+    inputLoginUsername.value = inputLoginPin.value = "";
+    inputLoginPin.blur();
+
+    //Updating UI
+    updateUI(currentAccount);
+  } else {
+    alert(
+      "Wrong Username/Password.. Please try again with correct credential's"
+    );
+  }
+});
+
+//Implementing Transfer Money:
+
+btnTransfer.addEventListener("click", function (event) {
+  event.preventDefault();
+  const amount = Number(inputTransferAmount.value);
+  const receiverAcc = accounts.find(
+    (account) => account.username === inputTransferTo.value
+  );
+  // console.log(receiverAcc);
+  inputTransferAmount.value = inputTransferTo.value = "";
+  inputTransferAmount.blur();
+  inputTransferTo.blur();
+  if (
+    receiverAcc &&
+    amount > 0 &&
+    currentAccount.balance >= amount &&
+    receiverAcc.username !== currentAccount.username
+  ) {
+    currentAccount.movements.push(-amount);
+    receiverAcc.movements.push(amount);
+    //Updating UI
+    updateUI(currentAccount);
+  } else {
+    alert(`Transaction Failed..!!⚠️ Please check if \n
+      1.You have enough balance \n
+      2.Receiver's name is valid \n
+      3.Ammount is greater than zero \n
+      Note:You cannot transfer money to self account`);
+  }
+
+  console.log(-amount, receiverAcc);
+});
+
+//Let's go and build all the remaining things
